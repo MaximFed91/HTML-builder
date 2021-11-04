e740762f0a88f87ad024ba64d6d6610d4797624c
Maksim <honestliar91@gmail.com>
2021-11-04 21:31:28 +0300

    fix

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -12,10 +12,8 @@ fs.readdir(pathFolder, {
                 const pathItem = path.join(pathFolder, item.name);
                 const itemObj = path.parse(pathItem);
                 fs.stat(pathItem).then(itemStats=>{
-                    const size = Math.floor(itemStats.size/1024) +'.'+ itemStats.size%1024; 
-                    console.log(`${itemObj.name} - ${itemObj.ext.slice(1)} - ${size}kb`);
+                    console.log(`${itemObj.name} - ${itemObj.ext.slice(1)} - ${itemStats.size}`);
                 });
-                
             }
         });
     });
