65d08b72c50837fa0b07996fcf29f525815b2b5e
Maksim <honestliar91@gmail.com>
2021-11-08 22:10:42 +0300

    fix 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,20 +5,19 @@ const stylePath = path.join(__dirname, 'styles');
 const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
 
 
-fs.writeFile(bundlePath, '');
-fs.readdir(stylePath, {
+(async function () {
+    await fs.writeFile(bundlePath, '');
+    const stlFils = await fs.readdir(stylePath, {
         withFileTypes: true
-    })
-    .then(data => {
-        data.forEach(item => {
-            if (item.isFile()) {
-                const pathItem = path.join(stylePath, item.name);
-                const itemExt = path.extname(pathItem);
-                if (itemExt === '.css') {
-                    fs.readFile(pathItem).then(data => {
-                        fs.appendFile(bundlePath, data);
-                    });
-                }
-            }
-        });
     });
+    for (const item of stlFils) {
+        if (item.isFile()) {
+            const pathItem = path.join(stylePath, item.name);
+            const itemExt = path.extname(pathItem);
+            if (itemExt === '.css') {
+              const data = await  fs.readFile(pathItem);
+                   await fs.appendFile(bundlePath, data);
+            }
+        }
+    }
+})();
